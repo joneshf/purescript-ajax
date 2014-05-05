6a0fae43b22ddb95b72bb8759a09258f50667109
joneshf <jones3.hardy@gmail.com>
2014-05-04 22:57:36 -0700

    Started changing to records.

--- src/Network/Ajax.purs
+++ src/Network/Ajax.purs
@@ -3,45 +3,25 @@ module Network.Ajax where
   import Control.Monad.Eff
 
   import qualified Data.Array as A
+  import Data.Either
 
   import Network.HTTP
 
-  foreign import data XHR :: !
+  foreign import data XHR :: *
+
+  foreign import data XHREff :: !
 
   -- Clean up signatures.
-  type XHRAjax = forall a eff. Eff (x :: XHR | eff) (Ajax a)
-  type EffAjax = forall a eff. Eff eff (Ajax a)
+  type XHRAjax = forall a eff. Eff (x :: XHREff | eff) Ajax
+  type EffAjax = forall a eff. Eff eff Ajax
 
   type URI = String
-
   -- This should be something like data Payload = JSON | XML | PlainText | ...
   type Payload = String
 
-  data Ajax a = GetRequest URI [Header]
-              | PostRequest URI [Header] Payload
-              | Response [Header] a
-
-  instance functorAjax :: Functor Ajax where
-    (<$>) f (Response h x) = Response h (f x)
-    (<$>) _ (GetRequest  u h)   = GetRequest  u h
-    (<$>) _ (PostRequest u h p) = PostRequest u h p
-
-  instance applyAjax :: Apply Ajax where
-    (<*>) (Response h f) (Response h' x) = Response (h ++ h') (f x)
-    -- This is obviously missing stuff, maybe this isn't actually an `Apply`?
-
-  instance bindAjax :: Bind Ajax where
-    (>>=) (Response h x) f = f x
-    (>>=) (GetRequest  u h)   _ = GetRequest  u h
-    (>>=) (PostRequest u h p) _ = PostRequest u h p
-    -- Should this instead dish off the request and work with the response?
-    -- (>>=) (GetRequest  u h) f = response (get  u >>= \x -> send x) >>= f
-    -- (>>=) (PostRequest u h) f = response (post u >>= \x -> send x) >>= f
-
-  instance applicativeAjax :: Applicative Ajax where
-    pure x = Response [] x
-
-  instance monadAjax :: Monad Ajax
+  type Ajax = Either Request Response
+  type Request = {verb :: Verb, uri :: URI, headers :: [Header], content :: Payload, xhr :: XHR}
+  type Response = {status :: StatusCode, headers :: [Header], content :: Payload, xhr :: XHR}
 
   data ReadyState = Done
                   | HeadersReceived
@@ -59,29 +39,23 @@ module Network.Ajax where
 
     (/=) rs              rs'             = not (rs == rs')
 
-  delete :: URI -> XHRAjax
-  delete = httpVerb DELETE
-
-  get :: URI -> XHRAjax
-  get = httpVerb GET
-
-  head :: URI -> XHRAjax
-  head = httpVerb HEAD
-
+  delete  :: URI -> XHRAjax
+  delete  = httpVerb DELETE
+  get     :: URI -> XHRAjax
+  get     = httpVerb GET
+  head    :: URI -> XHRAjax
+  head    = httpVerb HEAD
   options :: URI -> XHRAjax
   options = httpVerb OPTIONS
-
-  patch :: URI -> XHRAjax
-  patch = httpVerb PATCH
-
-  post :: URI -> XHRAjax
-  post = httpVerb POST
-
-  put :: URI -> XHRAjax
-  put = httpVerb PUT
+  patch   :: URI -> XHRAjax
+  patch   = httpVerb PATCH
+  post    :: URI -> XHRAjax
+  post    = httpVerb POST
+  put     :: URI -> XHRAjax
+  put     = httpVerb PUT
 
   httpVerb :: Verb -> URI -> XHRAjax
-  httpVerb = open xhr
+  httpVerb v u = pure $ Left {verb: v, uri: u, headers: [], content: "", xhr: newXhr}
 
   onreadystatechange :: XHRAjax -> (XHRAjax -> EffAjax) -> EffAjax
   onreadystatechange = on "onreadystatechange"
@@ -102,7 +76,7 @@ module Network.Ajax where
 
   currentTarget :: XHRAjax -> XHRAjax
   currentTarget = attr "currentTarget"
-  response      :: forall a. XHRAjax -> Ajax a
+  response      :: XHRAjax -> Response
   response      = attr "response"
   responseText  :: XHRAjax -> String
   responseText  = attr "responseText"
@@ -128,12 +102,18 @@ module Network.Ajax where
   setRequestHeader x (Header header value) =
     curry2 (attr "setRequestHeader" x) (show header) value
 
+  ajax2XHR :: Ajax -> XHR
+  ajax2XHR = either getXhr getXhr
+    where
+      getXhr :: forall r. {xhr :: XHR | r} -> XHR
+      getXhr o = o.xhr
+
   -- | FFI Calls
 
   foreign import attr
     "function attr(a) {\
     \  return function(x) {\
-    \    return x.currentTarget;\
+    \    return ajax2XHR(x).currentTarget;\
     \  }\
     \}" :: forall a. String -> XHRAjax -> a
 
@@ -141,7 +121,7 @@ module Network.Ajax where
     "function on(method) {\
     \  return function(x) {\
     \    return function(f) {\
-    \      x[method] = f;\
+    \      ajax2XHR(x)[method] = f;\
     \      return function() { return x; }\
     \    }\
     \  }\
@@ -152,7 +132,7 @@ module Network.Ajax where
     \  return function(verb) {\
     \    return function(uri) {\
     \      var verbStr = PS.Prelude.show(PS.Network_HTTP.showHTTPVerb())(verb);\
-    \      x.open(verbStr, uri);\
+    \      ajax2XHR(x).open(verbStr, uri);\
     \      return function() { return x; }\
     \    }\
     \  }\
@@ -160,14 +140,12 @@ module Network.Ajax where
 
   foreign import send
     "function send(x) {\
-    \  x.send();\
+    \  ajax2XHR(x).send();\
     \  return function() { return x; }\
     \}" :: XHRAjax -> XHRAjax
 
-  foreign import xhr
-    "function xhr() {\
-    \  return new XMLHttpRequest();\
-    \}" :: XHRAjax
+  foreign import newXhr
+    "var newXhr = new XMLHttpRequest();" :: XHR
 
   foreign import curry2
     "function curry2(f) {\
